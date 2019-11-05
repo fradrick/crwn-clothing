0453f56ecc312a8124f6f55fe4d96ee20a03db6a
charakuf <charakupaf@gmail.com>
2019-11-05 14:27:18 +0200

    added cart icon and cart dropdpwn component, implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,34 +7,40 @@ import { auth } from '../../firebase/firebase.util';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
-      </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
-      {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className='option' to='/signin'>
-          SIGN IN
+const Header = ({ currentUser, hidden }) => (
+    <div className='header'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo' />
         </Link>
-      )}
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                SHOP
+            </Link>
+            <Link className='option' to='/shop'>
+                CONTACT
+            </Link>
+            {currentUser ? (
+                <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+            ) : (
+                <Link className='option' to='/signin'>
+                    SIGN IN
+                </Link>
+            )}
+            <CartIcon/>
+        </div>
+        {
+            hidden ? null : <CartDropdown/>
+        }
     </div>
-  </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser, hidden
 });
 //currentUser is the one we shall pass to headerComponent above
 //state is the root reducer
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,9 @@
+import CartActionTypes from "./cart.types";
+
+
+export const toggleCartHidden = () => ({
+    type: CartActionTypes.TOOGLE_CART_HIDDEN
+});
+
+//payload is optional
+
